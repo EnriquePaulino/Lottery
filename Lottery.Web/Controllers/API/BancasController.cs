bcb224dc24c18849e0efb9a146470ce885fa6a00
EnriquePaulino <paulinoenrique@gmail.com>
2019-05-18 11:02:19 -0400

    API Banca Controller

---
+++ Lottery.Web/Controllers/API/BancasController.cs
@@ -0,0 +1,23 @@
+﻿namespace Lottery.Web.Controllers.API
+{
+    using Data;
+    using Lottery.Web.Data.Repositories;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[Controller]")]
+    public class BancasController : Controller
+    {
+        private readonly IBancaRepository bancaRepository;
+
+        public BancasController(IBancaRepository bancaRepository)
+        {
+            this.bancaRepository = bancaRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetBancas()
+        {
+            return this.Ok(this.bancaRepository.GetAll());
+        }
+    }
+}
